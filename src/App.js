955ad76609f6394a49f7048e126aa5d41341a6f4
hahaha <fan.zhao@clobotics.com>
2020-06-15 17:56:01 +0800

    code split

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Switch,Link } from 'react-router-dom';
+import TestContext from './pages/context/index';
+import TestRef from './pages/ref/index';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Link to='/context'>context</Link> 
+      <Link to='/ref'>ref</Link> 
+      <Switch>
+        <Route path="/context" component={TestContext} />
+        <Route path="/ref" component={TestRef} />
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/context/TestContext.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function TestContext(){
+    return (
+        <div>test Context</div>
+    )
+}
